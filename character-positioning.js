f07bf3fa543f8ae90a9feeca5be6106d404fd80c
Teri <teri.eich@gmail.com>
2018-11-28 18:55:04 +0000

    took out extra comment

--- character-positioning.js
+++ character-positioning.js
@@ -7,7 +7,7 @@ var letterList = {};
     for (i = 0; i < input.length; i++) {
       var letter = input[i];
       if (letter in letterList) {
-        letterList[letter].push(input.indexOf(letter, i)); //find index and add
+        letterList[letter].push(input.indexOf(letter, i));
       } else {
         letterList[letter] = [input.indexOf(letter)];
       }
@@ -19,10 +19,3 @@ var letterList = {};
 
 //test lines:
 console.log(findLetters("lighthouse in the House"));
-// l i g h t h o u s e i  n  t  h  e  h  o  u  s  e
-// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
-
-//get string
-//separate letters and make object keys
-//count instances of each letter - key: amt
-//add to object key value
